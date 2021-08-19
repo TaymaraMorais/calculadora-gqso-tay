7b0ec4431a13bfc9771fcf9d2fb2c72396bccb62
Taymara Morais <86384755+TaymaraMorais@users.noreply.github.com>
2021-08-19 16:30:15 -0300

    Criação da classe base da função soma

---
+++ src/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoraapi;
+
+/**
+ *
+ * @author Taymara
+ */
+public class Calculadora {
+    private double resultado = 0;
+    
+    public double soma(double valor1, double valor2){
+        this.resultado = valor1 + valor2;
+        return this.resultado;
+    }
+}
