a167b1c0247ad08a09e639fceaac8e02ccda4163
Taymara Morais <86384755+TaymaraMorais@users.noreply.github.com>
2021-12-01 17:20:01 -0300

    Criação dos outros dois testes unitários

---
+++ test/CalculadoraWebServiceTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoraapi;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cadu
+ */
+public class CalculadoraWebServiceTest {
+    
+    public CalculadoraWebServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSoma method, of class CalculadoraWebService.
+     */
+    @Test
+    public void testGetSoma() {
+        System.out.println("getSoma");
+        double valor1 = 0.0;
+        double valor2 = 0.0;
+        CalculadoraWebService instance = new CalculadoraWebService();
+        double expResult = 0.0;
+        double result = instance.getSoma(valor1, valor2);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    
+}
