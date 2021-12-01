2e1a5d4304a5cec07135b3e4f11edbf10f59a29e
Taymara Morais <86384755+TaymaraMorais@users.noreply.github.com>
2021-12-01 17:18:15 -0300

    Criação de um teste único

---
+++ test/CalculadoraTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoraapi;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cadu
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of soma method, of class Calculadora.
+     */
+    @Test
+    public void testSoma() {
+        System.out.println("soma");
+        double valor1 = 0.0;
+        double valor2 = 0.0;
+        Calculadora instance = new Calculadora();
+        double expResult = 0.0;
+        double result = instance.soma(valor1, valor2);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    
+}
